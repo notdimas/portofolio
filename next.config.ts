d46b26eb9d44fdaefeba3bd8967e26aaef510b3b
notdimas <dimasbagusprayogo17@gmail.com>
2025-04-16 11:42:36 +0700

    another netlify fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  images: {
-    unoptimized: true,
-    domains: [],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
